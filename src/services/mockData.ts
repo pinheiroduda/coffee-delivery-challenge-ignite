a1df0a0d2bcc6831031f71885010525ce195d9ef
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2023-01-20 08:24:57 -0300

    feat: creating cards data

---
+++ src/services/mockData.ts
@@ -0,0 +1,101 @@
+import Expresso from '../assets/Expresso.svg'
+import American from '../assets/American.svg'
+import CremyExpresso from '../assets/CreamyExpresso.svg'
+import IcedCoffee from '../assets/IcedCoffee.svg'
+import MilkyCoffee from '../assets/MilkyCoffee.svg'
+import Latte from '../assets/Latte.svg'
+import Capuccino from '../assets/Capuccino.svg'
+import Macchiato from '../assets/Macchiato.svg'
+import Mocaccino from '../assets/Mocaccino.svg'
+import HotChocolate from '../assets/HotChocolate.svg'
+import Cuban from '../assets/Cuban.svg'
+import Hawaiian from '../assets/Hawaiian.svg'
+import Arabic from '../assets/Arabic.svg'
+import Irish from '../assets/Irish.svg'
+
+export const coffeeCardsList = [
+  {
+    image: Expresso,
+    type: ['Tradicional'],
+    name: 'Expresso Tradicional',
+    description: 'O tradicional café feito com água quente e grãos moídos'
+  },
+  {
+    image: American,
+    type: ['Tradicional'],
+    name: 'Expresso Americano',
+    description: 'Expresso diluído, menos intenso que o tradicional'
+  },
+  {
+    image: CremyExpresso,
+    type: ['Tradicional'],
+    name: 'Expresso Cremoso',
+    description: 'Café expresso tradicional com espuma cremosa'
+  },
+  {
+    image: IcedCoffee,
+    type: ['Tradicional', 'Gelado'],
+    name: 'Expresso Gelado',
+    description: 'Bebida preparada com café expresso e cubos de gelo'
+  },
+  {
+    image: MilkyCoffee,
+    type: ['Tradicional', 'Com leite'],
+    name: 'Café com Leite',
+    description: 'Meio a meio de expresso tradicional com leite vaporizado'
+  },
+  {
+    image: Latte,
+    type: ['Tradicional', 'Com leite'],
+    name: 'Latte',
+    description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa'
+  },
+  {
+    image: Capuccino,
+    type: ['Tradicional', 'Com leite'],
+    name: 'Capuccino',
+    description: 'Bebida com canela feita de doses iguais de café, leite e espuma'
+  },
+  {
+    image: Macchiato,
+    type: ['Tradicional', 'Com leite'],
+    name: 'Macchiato',
+    description: 'Café expresso misturado com um pouco de leite quente e espuma'
+  },
+  {
+    image: Mocaccino,
+    type: ['Tradicional'],
+    name: 'Mocaccino',
+    description: 'Café expresso com calda de chocolate, pouco leite e espuma'
+  },
+  {
+    image: HotChocolate,
+    type: ['Especial', 'Gelado'],
+    name: 'Chocolate Quente',
+    description: 'Bebida feita com chocolate dissolvido no leite quente e café'
+  },
+  {
+    image: Cuban,
+    type: ['Especial', 'Alcoólico', 'Gelado'],
+    name: 'Cubano',
+    description: 'Drink gelado de café expresso com rum, creme de leite e hortelã'
+  },
+  {
+    image: Hawaiian,
+    type: ['Especial'],
+    name: 'Havaiano',
+    description: 'Bebida adocicada preparada com café e leite de coco'
+  },
+  {
+    image: Arabic,
+    type: ['Especial'],
+    name: 'Árabe',
+    description: 'Bebida preparada com grãos de café árabe e especiarias'
+  },
+  {
+    image: Irish,
+    type: ['Especial', 'Alcoólico'],
+    name: 'Irlandês',
+    description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly'
+  },
+]
