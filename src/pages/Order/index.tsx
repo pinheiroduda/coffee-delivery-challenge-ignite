d7849bf0436649de026e94bd41e9290bbb6b02a3
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2023-02-22 16:59:45 -0300

    feat: adds count funcionality to order page

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,16 +1,17 @@
-import { 
+import {
   Bank,
   CreditCard,
   CurrencyDollar,
   MapPinLine,
   Money,
   Trash
-} from "phosphor-react";
-import { useNavigate } from "react-router-dom";
-import { useTheme } from "styled-components";
-import { CounterButton } from "../../components/CounterButton";
+} from 'phosphor-react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useTheme } from 'styled-components'
+import { CounterButton } from '../../components/CounterButton'
 
-import { 
+import {
   Address,
   ButtonsBox,
   ConfirmOrderButton,
@@ -26,9 +27,10 @@ import {
   SectionTitleBox,
   TitleBox,
   TotalDetails
-} from "./styles";
+} from './styles'
 
 export function Order() {
+  const [counter, setCounter] = useState(1)
   const theme = useTheme()
   const navigate = useNavigate()
 
@@ -42,45 +44,63 @@ export function Order() {
         <TitleBox>Complete seu pedido</TitleBox>
         <Address>
           <SectionTitleBox>
-            <MapPinLine size={22} weight="regular" color={theme['yellow-800']}/>
+            <MapPinLine
+              size={22}
+              weight="regular"
+              color={theme['yellow-800']}
+            />
             <div>
               <h2>Endereço de entrega</h2>
               <p>Informe o endereço onde deseja receber seu pedido</p>
             </div>
           </SectionTitleBox>
           <form>
-            <input type="number" placeholder="CEP"/>
-            <input type="text"  className="fullSize" placeholder="Rua"/>
+            <input type="number" placeholder="CEP" />
+            <input type="text" className="fullSize" placeholder="Rua" />
             <div>
-              <input type="number" placeholder="Número"/>
-              <input type="text" className="bigSize" placeholder="Complemento"/>
+              <input type="number" placeholder="Número" />
+              <input
+                type="text"
+                className="bigSize"
+                placeholder="Complemento"
+              />
             </div>
             <div>
-              <input type="text" placeholder="Bairro"/>
-              <input type="text" placeholder="Cidade" className="mediumSize"/>
-              <input type="text" className="smallSize" placeholder="UF"/>
+              <input type="text" placeholder="Bairro" />
+              <input type="text" placeholder="Cidade" className="mediumSize" />
+              <input type="text" className="smallSize" placeholder="UF" />
             </div>
           </form>
         </Address>
         <PaymentSection>
           <SectionTitleBox>
-            <CurrencyDollar size={22} weight="duotone" color={theme['purple-400']}/>
+            <CurrencyDollar
+              size={22}
+              weight="duotone"
+              color={theme['purple-400']}
+            />
             <div>
               <h2>Pagamento</h2>
-              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
             </div>
           </SectionTitleBox>
           <PaymentOptions>
             <PaymentOptionsButton>
-              <CreditCard size={16} weight="duotone" color={theme['purple-400']}/>
+              <CreditCard
+                size={16}
+                weight="duotone"
+                color={theme['purple-400']}
+              />
               Cartão de crédito
             </PaymentOptionsButton>
             <PaymentOptionsButton>
-              <Bank size={16} weight="duotone" color={theme['purple-400']}/>
+              <Bank size={16} weight="duotone" color={theme['purple-400']} />
               Cartão de débito
             </PaymentOptionsButton>
             <PaymentOptionsButton>
-              <Money size={16} weight="duotone" color={theme['purple-400']}/>
+              <Money size={16} weight="duotone" color={theme['purple-400']} />
               Dinheiro
             </PaymentOptionsButton>
           </PaymentOptions>
@@ -95,15 +115,18 @@ export function Order() {
               <div>
                 <p>Nome do café</p>
                 <ButtonsBox>
-                  <CounterButton />
+                  <CounterButton
+                    counter={counter}
+                    onCounterChange={setCounter}
+                  />
                   <button>
-                    <Trash width={16} color={theme['purple-400']}/>
+                    <Trash width={16} color={theme['purple-400']} />
                     <p>Remover</p>
                   </button>
                 </ButtonsBox>
               </div>
             </Item>
-              <p>total</p>
+            <p>total</p>
           </ItemsDetails>
           <ItemsPriceDetails>
             <p>Total de itens</p>
